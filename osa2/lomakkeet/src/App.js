3aa2c28be722e326dc9802b954b40d9944ae3b01
Yusif Salam-zade <yusif.salam-zade@aalto.fi>
2018-08-27 16:40:48 +0300

    Exercise 2.8 done

--- osa2/lomakkeet/src/App.js
+++ osa2/lomakkeet/src/App.js
@@ -8,10 +8,12 @@ class App extends React.Component {
       persons: [
         {
           name: 'Arto Hellas',
-          id: 1
+          id: 1,
+          number: '040-123456'
         }
       ],
-      newName: ''
+      newName: '',
+      newNumber: ''
     }
   }
 
@@ -19,27 +21,33 @@ class App extends React.Component {
     event.preventDefault()
     const personObject = {
       name: this.state.newName,
-      date: new Date().toISOString,
+      number: this.state.newNumber,
+      date: new Date().toISOString(),
       id: this.state.persons.length + 1
     }
     if (this.state.persons.filter(person => person.name === personObject.name).length > 0) {
       alert("person already added!")
       this.setState({
         persons: this.state.persons,
-        newName: ''
+        newName: '',
+        newNumber: ''
       })
     }
     else {
       const persons = this.state.persons.concat(personObject)
       this.setState({
         persons: persons,
-        newName: ''
+        newName: '',
+        newNumber: ''
       })
     }
   }
 
-  handlePersonChange = (event) => {
+  handlePersonName = (event) => {
     this.setState({ newName: event.target.value })
+  }
+  handlePersonNumber = (event) => {
+    this.setState({newNumber: event.target.value})
   }
 
   render() {
@@ -49,7 +57,10 @@ class App extends React.Component {
         <h2>Puhelinluettelo</h2>
         <form>
           <div>
-            nimi: <input onChange={this.handlePersonChange} value = {this.state.newName}/>
+            nimi: <input onChange={this.handlePersonName} value = {this.state.newName}/>
+          </div>
+          <div>
+            numero: <input onChange={this.handlePersonNumber} value = {this.state.newNumber}/>
           </div>
           <div>
             <button type="submit" onClick={this.addPerson}>lisää</button>
