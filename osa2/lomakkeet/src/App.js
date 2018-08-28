990346945bdccf26d115f447b6e2c473b9c6ba8a
Yusif Salam-zade <yusif.salam-zade@aalto.fi>
2018-08-28 14:46:24 +0300

    Exercise 2.11 done

--- osa2/lomakkeet/src/App.js
+++ osa2/lomakkeet/src/App.js
@@ -2,21 +2,25 @@ import React from 'react'
 import Person from './components/Person'
 import Search from './components/Search'
 import PersonForm from './components/PersonForm'
+import axios from 'axios'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      persons: [
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Martti Tienari', number: '040-123456' },
-        { name: 'Arto Järvinen', number: '040-123456' },
-        { name: 'Lea Kutvonen', number: '040-123456' }
-      ],
+      persons: [],
       newName: '',
       newNumber: '',
       filter: ''
     }
+  }
+
+  componentDidMount(){
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        this.setState({persons: response.data})
+      })
   }
 
   addPerson = (event) => {
