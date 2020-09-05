21b734410801c1f83dd9a9ec1e6fd2d54406568c
Yusif Salam-zade <yusalam@gmail.com>
2020-09-05 22:43:01 +0300

    9.5

--- osa9/bmi/bmiCalculator.ts
+++ osa9/bmi/bmiCalculator.ts
@@ -1,27 +1,30 @@
-const calculateBmi = (height: number, mass: number): string => {
-  const bmi = mass / (height / 100) ** 2
-  switch (true) {
-    case bmi < 15:
-      return 'Very severely underweight'
-    case bmi < 16:
-      return 'Severely underweight'
-    case bmi < 18.5:
-      return 'Underweight'
-    case bmi < 25:
-      return 'Normal (healthy weight)'
-    case bmi < 30:
-      return 'Overweight'
-    case bmi < 35:
-      return 'Obese Class I (Moderately obese)'
-    case bmi < 40:
-      return 'Obese Class II (Severely obese)'
-    case bmi >= 40:
-      return 'Obese Class III (Very severely obese)'
-    default:
-      throw new Error('Something went wrong!')
-  }
+export const calculateBmi = (height: number, mass: number): string => {
+    const bmi: number = mass / (height / 100) ** 2
+    switch (true) {
+        case bmi < 15:
+            return 'Very severely underweight'
+        case bmi < 16:
+            return 'Severely underweight'
+        case bmi < 18.5:
+            return 'Underweight'
+        case bmi < 25:
+            return 'Normal (healthy weight)'
+        case bmi < 30:
+            return 'Overweight'
+        case bmi < 35:
+            return 'Obese Class I (Moderately obese)'
+        case bmi < 40:
+            return 'Obese Class II (Severely obese)'
+        case bmi >= 40:
+            return 'Obese Class III (Very severely obese)'
+        default:
+            throw new Error('Something went wrong!')
+    }
 }
 
-const a: number = Number(process.argv[2])
-const b: number = Number(process.argv[3])
-console.log(calculateBmi(a, b))
+const runningAsScript = !module.parent
+if (runningAsScript) {
+    const a: number = Number(process.argv[2])
+    const b: number = Number(process.argv[3])
+    console.log(calculateBmi(a, b))
+}
--- osa9/bmi/index.ts
+++ osa9/bmi/index.ts
@@ -1,8 +1,23 @@
 import express from 'express'
+import { calculateBmi } from './bmiCalculator'
 const app = express()
 
 app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack')
+})
+
+app.get('/bmi', (req, res) => {
+    if (!req.query.weight || !req.query.height)
+        res.json({ error: 'malformatted parameters' })
+    else {
+        const weight = Number(req.query.weight)
+        const height = Number(req.query.height)
+        const bmi = calculateBmi(height, weight)
+        if (bmi === 'Error!') res.json({ error: 'malformatted parameters' })
+        else {
+            res.json({ weight, height, bmi })
+        }
+    }
 })
 
 const PORT = 3003
