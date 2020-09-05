462461e8a7fa998894b3629c0d45615c52caa1bc
Yusif Salam-zade <yusalam@gmail.com>
2020-09-05 16:23:22 +0300

    9.3

--- osa9/bmi/exerciseCalculator.ts
+++ osa9/bmi/exerciseCalculator.ts
@@ -8,12 +8,12 @@ interface Result {
     average: number
 }
 
-const calculateExercise = (days: Array<number>, target: number): Result => {
+const calculateExercise = (target: number, days: Array<number>): Result => {
     const daysCount = days.length
     const trainingDays = daysCount - days.filter((day) => day === 0).length
     const average = days.reduce((a, b) => a + b) / daysCount
     const ratio = average / target
-    const rating = ratio <= 0.9 ? 0 : ratio <= 1 ? 1 : ratio <= 1.1 ? 2 : 3
+    const rating = ratio <= 1 ? 1 : ratio <= 1.1 ? 2 : 3
     const ratingDescription =
         rating === 1
             ? 'try trying'
@@ -33,4 +33,31 @@ const calculateExercise = (days: Array<number>, target: number): Result => {
     }
 }
 
-console.log(calculateExercise([3, 5, 2, 4.5, 0, 3, 1], 2))
+interface ParsedArgs {
+    target: number
+    arr: Array<number>
+}
+
+const parseArgs = (args: Array<string>): ParsedArgs => {
+    if (args.length < 4) throw new Error('Not enough arguments!')
+    const arguments = args.slice(2)
+    const target = Number(arguments[0])
+    const arr = new Array<number>()
+    const arrNumbers = arguments.slice(1).map((n) => Number(n))
+    const arrValid = arrNumbers.filter((n) => isNaN(n)).length === 0
+    if (!isNaN(target) && arrValid) {
+        return {
+            target,
+            arr: arrNumbers,
+        }
+    } else {
+        throw new Error('Provided values are not numbers')
+    }
+}
+
+try {
+    const { target, arr } = parseArgs(process.argv)
+    console.log(calculateExercise(target, arr))
+} catch (e) {
+    console.log('Something went wrong, message: ', e.message)
+}
